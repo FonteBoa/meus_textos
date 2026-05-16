5a885f6fff76f2929ed2690ab4dcbca2b3741354
Sr. Luiz Fonte Boa <fonteboa@gmail.com>
2026-05-16 10:41:58 -0300

    publica: 2 novo(s)

--- _publicador/3-PUBLICAR.bat
+++ _publicador/3-PUBLICAR.bat
@@ -1,15 +1,7 @@
 @echo off
-chcp 65001 >nul
-title fonteboa — publicando...
-
-:: Garante que o Git não interrompe com perguntas
+title fonteboa publicando...
 git config --global gc.auto 0
-
-:: A pasta do site é a pasta acima desta (_publicador)
 set SITE_DIR=%~dp0..
-
-:: Roda o publicador
 node "%~dp0publicar.js" "%SITE_DIR%"
-
 echo.
 pause
