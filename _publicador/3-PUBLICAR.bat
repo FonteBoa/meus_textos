d45d0c0eeeac66a0e72c5b33298cbd6af07f1dc6
Sr. Luiz Fonte Boa <fonteboa@gmail.com>
2026-05-16 10:35:53 -0300

    publica: 2 novo(s)

--- _publicador/3-PUBLICAR.bat
+++ _publicador/3-PUBLICAR.bat
@@ -2,6 +2,9 @@
 chcp 65001 >nul
 title fonteboa — publicando...
 
+:: Garante que o Git não interrompe com perguntas
+git config --global gc.auto 0
+
 :: A pasta do site é a pasta acima desta (_publicador)
 set SITE_DIR=%~dp0..
 
