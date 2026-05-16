fb5e5b7504197b054f68e3bad2a9f4b82c50410f
Sr. Luiz Fonte Boa <fonteboa@gmail.com>
2026-05-16 11:14:11 -0300

    publica: 1 novo(s)

--- _publicador/3-PUBLICAR.bat
+++ _publicador/3-PUBLICAR.bat
@@ -1,7 +1,12 @@
 @echo off
 title fonteboa publicando...
 git config --global gc.auto 0
+git config --global gc.autopacklimit 0
+git config --global gc.pruneExpire never
 set SITE_DIR=%~dp0..
 node "%~dp0publicar.js" "%SITE_DIR%"
+echo.
+echo Se aparecerem perguntas acima, tecle Ctrl+C para sair.
+echo O texto ja foi publicado com sucesso.
 echo.
 pause
